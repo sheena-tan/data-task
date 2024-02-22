390025f8dd6790078b76242ee20e1e183a76fbfc
Sheena Tan <sheenatan2024@u.northwestern.edu>
2024-02-21 22:45:50 -0600

    writeup of initial data exploration

--- 0_initial_setup.R
+++ 0_initial_setup.R
@@ -1,13 +1,6 @@
 #### Initial Setup ----
 #### data cleaning, eda
 
-# Notes: There definitely seems to be a significant difference between the feelings
-# of those who do and don't get return apologies (Task1). There also seems to be
-# significant differences between the different types of people (Task2). I wonder if this
-# maps onto agreeableness personality-wise. Could also be more related to people's
-# lived experiences and morals?
-
-
 library(tidyverse)
 library(here)
 library(patchwork)
@@ -34,10 +27,12 @@ apology_clean <- apology_data |>
 plot_density_boxplot <- function(data, x_variable) {
   p1 <- ggplot(data, aes({{x_variable}})) +
     geom_boxplot() +
+    xlim(-30, 30) +
     theme_void()
 
   p2 <- ggplot(data, aes({{x_variable}})) +
     geom_density() +
+    xlim(-30, 30) +
     theme_minimal() +
     theme(
       axis.text.y = element_blank(),
@@ -48,35 +43,45 @@ plot_density_boxplot <- function(data, x_variable) {
   p1/p2 + plot_layout(heights = unit(c(1, 5), c("cm", "null")))
 }
 
-plot_density_boxplot(apology_clean, feelings_youalone)
+alone_total <- plot_density_boxplot(apology_clean, feelings_youalone)
 # overall, people feel very bad (-30,-10) when they do not get a return apology
-plot_density_boxplot(apology_clean, feelings_bothyoufirst)
+both_total <- plot_density_boxplot(apology_clean, feelings_bothyoufirst)
 # overall, people feel much better (0,20) when they do get a return apology
 
 
-apology_clean |> filter(initiator_type == "always") |>
+alone_always <- apology_clean |> filter(initiator_type == "always") |>
   plot_density_boxplot(feelings_youalone)
 # always-type do not feel good (-20,-10) with no return apology, but better than the population average
-apology_clean |> filter(initiator_type == "always") |>
+both_always <- apology_clean |> filter(initiator_type == "always") |>
   plot_density_boxplot(feelings_bothyoufirst)
 # always-type feel much better (10,20) when both people apologize
 
 
-apology_clean |> filter(initiator_type == "conditional") |>
+alone_cond <- apology_clean |> filter(initiator_type == "conditional") |>
   plot_density_boxplot(feelings_youalone)
 # dramatic negative shift strong right skew, feel horrible (-30,-23) with no return apology
-apology_clean |> filter(initiator_type == "conditional") |>
+both_cond <- apology_clean |> filter(initiator_type == "conditional") |>
   plot_density_boxplot(feelings_bothyoufirst)
 # significantly more positive (-8,20) with apology, though not as positive as always-type
 
 
-apology_clean |> filter(initiator_type == "never") |>
+alone_never <- apology_clean |> filter(initiator_type == "never") |>
   plot_density_boxplot(feelings_youalone)
 # wide but negative spread (-30,-12) with no apology
-apology_clean |> filter(initiator_type == "never") |>
+both_never <- apology_clean |> filter(initiator_type == "never") |>
   plot_density_boxplot(feelings_bothyoufirst)
 # interestingly left skew (-8,10) but least positive out of all types
 
+
+# write plots
+ggsave(here("results/alone_total.png"), alone_total)
+ggsave(here("results/both_total.png"), both_total)
+ggsave(here("results/alone_always.png"), alone_always)
+ggsave(here("results/both_always.png"), both_always)
+ggsave(here("results/alone_cond.png"), alone_cond)
+ggsave(here("results/both_cond.png"), both_cond)
+ggsave(here("results/alone_never.png"), alone_never)
+ggsave(here("results/both_never.png"), both_never)
 
 
 
---
+++ 1_task1.R
@@ -0,0 +1,2 @@
+#### Task 1 ----
+#### feelings with/without apology, variation by initiator type, forgiveness
