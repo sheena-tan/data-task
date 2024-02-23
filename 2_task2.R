1ee0280c81790da6ae1346715ac5f9d6e3c3a980
Sheena Tan <sheenatan2024@u.northwestern.edu>
2024-02-22 18:27:23 -0600

    task 2 part c

--- 2_task2.R
+++ 2_task2.R
@@ -86,11 +86,39 @@ t.test(formula = value ~ scenario, data = apology_b5, paired = TRUE)
 
 
 # Part C
-# Create a graph showing the proportion of people choosing each of the different options for the following variable: outcome_binary1
-# Conduct a test to determine if the proportion differences across the answers are significantly different from one another.
+# data transformation
+apology_c <- apology_clean |>
+  group_by(outcome_binary1) |>
+  summarize(n = n()) |>
+  mutate(prop = n / sum(n))
+
+# bar graph of proportions
+bar_graph_outcome <- apology_c |>
+  ggplot(aes(outcome_binary1, prop, fill = outcome_binary1)) +
+  geom_bar(stat = "identity") +
+  theme_ggdist() +
+  scale_fill_manual(values = c("#d95f02", "#1b9e77")) +
+  theme(
+    plot.subtitle = element_markdown(lineheight = 0.1),
+    legend.position = "none"
+  ) +
+  scale_x_discrete(labels = c("you first", "neither")) +
+  annotate("text", x = 1, y = 0.4, label = "0.783", color = "white") +
+  annotate("text", x = 2, y = 0.1, label = "0.217", color = "white") +
+  labs(
+    x = "apology scenario",
+    y = "proportion of participants",
+    subtitle = "Participants preferred <b style='color: #d95f02;'>apologizing first</b>
+    over <b style='color: #1b9e77;'>no one apologizing</b> at all."
+  )
+
+# one prop z test
+prop.test(x = 36, n = 46, p = 0.5, correct = FALSE)
 
 # Part D
 # Find a way to analyze the sentiment and/or emotions present in the free form text responses in the “describe” variable. Describe your observations in a few sentences.
 
 # write out plots
 ggsave(here("results/bar_graph_desc.png"), bar_graph_desc)
+ggsave(here("results/bar_graph_outcome.png"), bar_graph_outcome)
+
